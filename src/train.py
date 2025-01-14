90ca2bdeaff3d7b827b22124323753f5571a3d50
DHIMOILA Gregoire <gregoire.dhimoila@inria.fr>
2025-01-14 17:37:51 +0100

    hehe

--- src/train.py
+++ src/train.py
@@ -1,21 +1,30 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
+from pathlib import Path
+import numpy as np
+import torch as th
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
 # Now is the floor is yours to implement the agent and train it.
 
-
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+
+SAVE_PATH = Path(__file__).parent.parent / "output"
+MODEL_NAME = "final.pkl"
+
+
 class ProjectAgent:
     def act(self, observation, use_random=False):
-        return 0
+        observation = np.log(np.maximum(observation, 1e-8))
+        return self.policy.predict(observation, deterministic=True)[0]
 
     def save(self, path):
-        pass
+        th.save(self.policy, path)
 
     def load(self):
-        pass
+        self.policy = th.load(SAVE_PATH / MODEL_NAME, weights_only=False)
